1937c31f8958ff0d04918cfdb967adb4b6021690
dapocalypse <dedhiadev5@gmail.com>
2025-01-25 23:04:41 -0600

    Creates a CSV parser

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 <!-- HEAD section ............................................................................ -->
 <head>
-  <title> Yoonsuck Choe's Experimental Web Site </title>
+  <title> Jackie's Experimental Web Site </title>
 
 
   <!-- javascript functions -->
@@ -42,94 +42,26 @@
 
 <!-- PHP testing area ................................ --> 
 <?php
+   require_once("proc_csv.php");
 
-   echo "<h1> Choe's Experimental CSCE 331 Docker Web Site </h1>\n";
+   echo "<h1> Jackie's Experimental CSCE 331 Docker Web Site </h1>\n";
 
-   echo "<font color=\"green\"> Haha update and redeploy </font><p/>\n";
-   echo " Testing PHP <br>\n";
-   echo " Hello world!<p/>\n";
+   echo "<h3>CSV Parser Testing</h3>\n";
 
-   echo "<h3>Testing file loading:</h3>\n";
+   echo "<h4>Selected Columns</h4>\n";
+   proc_csv("dat-doublequote-comma.csv", ",", "\"", "1:3");
+   proc_csv("dat-doublequote-tab.csv", "\t", "\"", "1:3");
+   proc_csv("dat2-doublequote-comma.csv", ",", "\"", "1:3");
+   proc_csv("dat2-doublequote-tab.csv", "\t", "\"", "1:3");
+   proc_csv("dat2-singlequote-tab.csv", "\t", "'", "1:3");
 
-   # FILE access 
-   # $h = fopen("false.dat","r");
-
-   $handle = fopen("data.dat","r") or die("Cannot open data.dat");
-
-   echo "<table  border=\"1\">\n";
-
-   while ($data = fgets($handle)) {
-        echo "<tr>\n";
-        $data_cols = preg_split('/,/',$data);
-        for ($k=0; $k<count($data_cols); ++$k) {
-            echo "  <td> ".$data_cols[$k]." </td>\n";
-        }
-        echo "</tr>\n";
-   }
-
-   fclose($handle);
-
-   echo "</table>\n<p/>";
-   
-
-   echo "Debug: ";
-   print_r($data_cols); 
-
-   echo "<p/>";
-
-   echo "rand : ".$data_cols[rand(0,1)]."\n";
-
-   echo "<p/>";
-
+   echo "<h4>All Columns</h4>\n";
+   proc_csv("dat-doublequote-comma.csv", ",", "\"", "ALL");
+   proc_csv("dat-doublequote-tab.csv", "\t", "\"", "ALL");
+   proc_csv("dat2-doublequote-comma.csv", ",", "\"", "ALL");
+   proc_csv("dat2-doublequote-tab.csv", "\t", "\"", "ALL");
+   proc_csv("dat2-singlequote-tab.csv", "\t", "'", "ALL");
 ?>
-
-
-<!-- Java script testing area ............................... -->
-
-<div class="secondaryFont"> 
-
-<h3>Java script test</h3>
-
-<p id="demo"> Content to be changed: </p> 
-
-<button type="button" onclick="randText()">Click Me!</button>
-
-<button onClick="window.location.reload();">Reload Page</button>
-
-<p>For more javascript examples, see <a href="jstest.php">jstest.php</a>.</p>
-
-</div>
-
-<!-- Fetch testing............................... -->
-<div class="SecondaryFont">
-<h3> Fetch html source code </h3>
-<a href="fetch.php">fetch.php</a>
-</div>
-
-<!-- HTML form input handling .......................... -->
-
-<h3>HTML Form input test</h3>
-<p/>
-<form action="action.php" method="post">
-Search: <input type="text" name="name"> 
-<input type="submit">
-</form>
-<p/>
-
-<h3>HTML Form input test 2 </h3>
-
-Search academic genealogy (external link: <a href="https://www.mathgenealogy.org">https://www.mathgenealogy.org</a>): <p/>
-<form action="https://www.mathgenealogy.org/query-prep.php" method="post">
-Firstname:
-<input type="text" name="given_name" value="Yoonsuck">  <br/>
-Lastname:
-<input type="text" name="family_name" value="Choe"> 
-<input type="submit">
-</form>
-<p/>
-</div> <!-- end of big div -->
-
-<?php echo "Container IP Address:".getenv('MY_IP')."\n"; ?>
 </body>
 
 </html>
---
+++ public/proc_csv.php
@@ -0,0 +1,43 @@
+<?php
+
+function proc_csv ($filename, $delimiter, $quote, $columns_to_show) {
+    $handle = fopen("../data/$filename", "r") or die("Cannot open " + $filename);
+
+    echo "<table border=\"1\"> \n";
+
+    $regex = "/(?:^|$delimiter)((?:'([^']*)')|(?:\"[^\"]*\")|(\d+))/";
+
+    $columnNumber = 1;
+    $columnArray = explode(":", $columns_to_show);
+
+    while ($data = fgets($handle)) {
+        echo "<tr>\n";
+
+        $items = [];
+
+        preg_match_all($regex, trim($data), $items);
+
+        foreach($items[1] as $item) {
+            if (substr($item,0, 1) == $quote &&
+                substr($item,-1) == $quote) {
+                $item = substr($item,1, strlen($item)-2);
+            }
+
+            $isSelectedColumn = false;
+
+            if ($columns_to_show == "ALL" ||
+                in_array($columnNumber, $columnArray)) {
+                echo "<td>$item</td>\n";
+            }
+        }
+        echo "</tr>\n<br>";
+
+        $columnNumber++;
+    }
+
+    fclose($handle);
+
+    echo "</table>\n";
+}
+
+?>
